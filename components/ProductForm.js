968d34584df2d8456cd541e8303d1c0e5b84f899
mountither <monti_123@live.com>
2021-12-28 17:45:12 +1100

    added product page form ui

---
+++ components/ProductForm.js
@@ -0,0 +1,63 @@
+import { useContext, useState } from "react";
+import { currencyFormatter } from "../utils/helpers";
+import ProductOptions from "./ProductOptions";
+
+const ProductForm = ({ product }) => {
+  const allVariantOptions = product.variants.edges?.map((variant) => {
+    const allOpts = {};
+
+    variant.node.selectedOptions.map((item) => {
+      allOpts[item.name] = item.value;
+    });
+
+    return {
+      id: variant.node.id,
+      title: product.title,
+      handle: product.handle,
+      image: variant.node.image?.originalSrc,
+      options: allOpts,
+      variantTitle: variant.node.title,
+      variantPrice: variant.node.priceV2.amount,
+      variantQuantity: 1,
+    };
+  });
+
+  const defaultValues = {};
+
+  product.options.map((item) => {
+    defaultValues[item.name] = item.values[0];
+  });
+
+  const [selectedVariant, setSelectedVariant] = useState(allVariantOptions[0]);
+  const [selectedOptions, setSelectedOptions] = useState(defaultValues);
+
+  const setOptions = (name, value) => {
+    setSelectedOptions((prevState) => {
+      return { ...prevState, [name]: value };
+    });
+  };
+  return (
+    <div className="rounded-2xl p-4 shadow-lg flex flex-col w-full md:w-1/3">
+      <h2 className="text-3xl font-bold">{product.title}</h2>
+      <span className="pb-6">
+        {currencyFormatter.format(
+          product.variants.edges[0].node.priceV2.amount
+        )}
+      </span>
+
+      {product.options.map(({ name, values }) => (
+        <ProductOptions
+          key={`name-${name}`}
+          name={name}
+          values={values}
+          selectedOptions={selectedOptions}
+          optionsHandler={setOptions}
+        />
+      ))}
+    
+        <button className="bg-black rounded-lg text-white px-2 py-3 hover:bg-gray-700">Add To Cart</button>
+    </div>
+  );
+};
+
+export default ProductForm;
--- pages/products/[product].js
+++ pages/products/[product].js
@@ -3,7 +3,7 @@ import { getAllProducts, getProduct } from "../../lib/shopify";
 
 const Product = ({ product }) => {
   return (
-    <div>
+    <div className="min-h-screen py-12 sm:pt-20">
       <ProductContent product={product} />
     </div>
   );
